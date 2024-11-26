55b28b08c2ebc6557ed54ebc21bd9bfb499cc58e
Wellison Ferreira <wellison.fdsilva@gmail.com>
2024-11-26 19:45:43 -0300

    Update pagamento2.php
    apenas estilização da pagina

--- code/pagamento2.php
+++ code/pagamento2.php
@@ -41,9 +41,53 @@ if (isset($_POST['id_cliente']) && !empty($_POST['id_cliente'])) {
     <title>Selecionar Empréstimos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/style.css" />
+    
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            margin: 0;
+        }
+
+        .navbar {
+            background-color: #343a40;
+        }
+
+        .navbar-brand {
+            font-weight: bold;
+            font-size: 1.5rem;
+        }
+
+        .navbar-nav .nav-link:hover {
+            background-color: #495057;
+            border-radius: 5px;
+        }
+
+        .footer {
+            margin-top: auto;
+            padding: 20px 0;
+            background-color: #343a40;
+            color: white;
+            width: 100%;
+        }
+    </style>
+
 </head>
 
 <body>
+
+ <!-- Navbar -->
+ <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">Carromeu e Julieta</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+
 
 <div class="container mt-5">
     <h3 class="text-center mb-4">Selecionar Empréstimo</h3>
@@ -86,6 +130,10 @@ if (isset($_POST['id_cliente']) && !empty($_POST['id_cliente'])) {
     </div>
 </div>
 
+<!-- Footer -->
+<footer class="footer text-center">
+        <p>© 2024 Carromeu e Julieta - Todos os direitos reservados</p>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
